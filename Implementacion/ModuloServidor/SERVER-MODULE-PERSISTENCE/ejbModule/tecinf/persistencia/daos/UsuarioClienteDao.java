50f65d8bd70317c46a6af592ceeb82b789b553e3
MRodriguez08 <mau.rod.81090@gmail.com>
2013-10-02 13:21:05 -0300

    nuevos daos

---
+++ Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/daos/UsuarioClienteDao.java
@@ -0,0 +1,11 @@
+package tecinf.persistencia.daos;
+
+import tecinf.persistencia.entities.UsuarioClienteEntity;
+
+public interface UsuarioClienteDao extends Dao<String , UsuarioClienteEntity>  {
+	
+	public UsuarioClienteEntity findById(String id);
+	
+	public UsuarioClienteEntity findByUserAndPassword(String id, String pwd);
+	
+}
--- Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/utiles/scripts/datos_iniciales.sql
+++ Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/utiles/scripts/datos_iniciales.sql
@@ -36,3 +36,23 @@ Insert into auditoria_operacion (id , descripcion) values (6 , 'Publicación');
 -- Objetos definidos para la auditoria
 Insert into auditoria_objeto (id , descripcion) values (1 , 'Usuario');
 Insert into auditoria_objeto (id , descripcion) values (2 , 'Contenido');
+
+--- USUARIOS DE PRUEBA
+
+--PELO
+insert into usuarios (usuario,apellidos,contrasenia,correo_electronico,fecha_nacimiento,nombres,telefono_movil,id_estado,id_sexo)
+values('pelo8888','fontes','cd1b67cecf6581172a9b39e5c1155824','pelo8888@gmail.com','1980-06-16','alejandro','095632145','H',1);
+insert into usuarios_cliente (id,ruta_imagen_perfil,id_tipo_registro,fk_usuario) 
+values('pelo8888','ruta',1,'pelo8888');
+
+--ANDRES
+insert into usuarios (usuario,apellidos,contrasenia,correo_electronico,fecha_nacimiento,nombres,telefono_movil,id_estado,id_sexo)
+values('aaldao','aldao','fb10b586a378f8abc224f8390346be6d','aaldao@gmail.com','1980-06-16','andres','095874587','H',1);
+insert into usuarios_cliente (id,ruta_imagen_perfil,id_tipo_registro,fk_usuario) 
+values('aaldao','ruta',1,'aaldao');
+
+--MAURICIO
+insert into usuarios (usuario,apellidos,contrasenia,correo_electronico,fecha_nacimiento,nombres,telefono_movil,id_estado,id_sexo)
+values('mrodriguez','rodriguez','08ed5c4b5499407be0a438654984da36','mau.rod.81090@gmail.com','1980-06-16','mauricio','099321458','H',1);
+insert into usuarios_cliente (id,ruta_imagen_perfil,id_tipo_registro,fk_usuario) 
+values('mrodriguez','ruta',1,'mrodriguez');
