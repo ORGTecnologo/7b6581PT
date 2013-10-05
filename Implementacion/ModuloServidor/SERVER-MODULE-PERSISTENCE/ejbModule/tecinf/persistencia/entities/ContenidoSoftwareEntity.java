708cd563cd6d58895dac7267e18d723267d4f018
MRodriguez08 <mau.rod.81090@gmail.com>
2013-10-04 21:45:35 -0300

    clases nuevas

--- Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/entities/ContenidoSoftwareEntity.java
+++ Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/entities/ContenidoSoftwareEntity.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Column;
-import javax.persistence.Id;
+import javax.persistence.Id; 
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
--- Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/entities/ContenidoVideoEntity.java
+++ Implementacion/ModuloServidor/SERVER-MODULE-PERSISTENCE/ejbModule/tecinf/persistencia/entities/ContenidoVideoEntity.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Column;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumn; 
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
