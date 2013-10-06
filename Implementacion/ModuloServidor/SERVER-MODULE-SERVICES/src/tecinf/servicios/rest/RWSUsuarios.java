f062008cf0b63d50d652bd13fe725311ff7c9464
MRodriguez08 <mau.rod.81090@gmail.com>
2013-10-06 20:51:30 -0200

    listar usuario arreglado

--- Implementacion/ModuloServidor/SERVER-MODULE-SERVICES/src/tecinf/servicios/rest/RWSUsuarios.java
+++ Implementacion/ModuloServidor/SERVER-MODULE-SERVICES/src/tecinf/servicios/rest/RWSUsuarios.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -43,7 +44,8 @@ public class RWSUsuarios {
  
 	}
 	
-	@GET
+	//@GET
+	@POST
 	@Path("/loginCliente")
 	@Produces("application/json")
 	@Consumes("application/json")
